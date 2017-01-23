17e9682957dad129ad224ef9b29716a78a1941a1
Mykhailo Obelchak <mykhailoobelchak@Mykhailos-Mac.local>
2017-01-23 16:12:50 -0500

    Initial Commit

---
+++ Calculator300907244/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator300907244
+//
+//  Created by Mykhailo Obelchak on 2017-01-23.
+//  Copyright © 2017 mykhailo.obelchak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
